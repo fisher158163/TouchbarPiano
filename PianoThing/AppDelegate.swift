26bd6742c749f4c1787d6b5c815ec43a17444782
Felix Luciano Salomon <felixluciano.a@gmail.com>
2020-01-01 14:47:54 -0500

    Initial Commit

---
+++ PianoThing/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PianoThing
+//
+//  Created by Felix Luciano on 1/1/20.
+//  Copyright © 2020 Felix Luciano. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PianoThing/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PianoThing
+//
+//  Created by Felix Luciano on 1/1/20.
+//  Copyright © 2020 Felix Luciano. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
